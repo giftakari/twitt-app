7af44c682555bd9b4623c8e1c0f15f809112c42a
Godsgift Akari <giftakari@yahoo.com>
2019-04-18 19:22:21 +1000

    Complete Project

--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
+import faker from "faker";
 
-const CommentDetails =()=>{
-  const firstName =faker.name.findName();
-  const lastName= faker.name.lastName();
-  
-return(
-<div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+const CommentDetails = props => {
+  const firstName = faker.name.firstName;
+  const lastName = faker.name.lastName;
+
+  const text = faker.lorem.sentence;
+
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {firstName()} {lastName()}
         </a>
-        <div className="content">
-           <a href="/" className ="author">
-            {firstName} {lastName}
-           </a>
-           <div className ="metadata">
-             <span className="date">Nice Post</span>
-            </div>
-          
-           <div className="text">{faker.lorem.sentence()}</div>
-
+        <div className="metadata">
+          <span className="date">{props.data}</span>
         </div>
 
+        <div className="text">{text()}</div>
       </div>
-);
-}
+    </div>
+  );
+};
 
 export default CommentDetails;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetails from './CommentDetails';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
+import './App.css';
 
 
-const App =() =>{
+const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails/>
-      <CommentDetails/>
-      <CommentDetails />
+      <ApprovalCard >
+        <CommentDetails date="Today @ 2.55pm" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails date="Yesterday @ 5:00am" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails date=" 2days ago @ 3:33am" />
+      </ApprovalCard>
     </div>
-  )
-} 
+  );
+};
 
-
-ReactDOM.render(<App/>, document.getElementById('root'));
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
